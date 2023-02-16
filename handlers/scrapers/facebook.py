4ceca60a93a66485599b79e6b6c95bedae8e09a1
ricardxalvarez <ricardoandresalvarez62341@gmail.com>
2023-02-15 20:35:13 -0400

    changes

--- handlers/scrapers/facebook.py
+++ handlers/scrapers/facebook.py
@@ -110,8 +110,12 @@ class Facebook():
             emit("message", {"status": True, "content": "Listing friends..."})
             browser.set_window_size(900, 700)
             time.sleep(float(decimal.Decimal(random.randrange(1, 2))))
-            browser.find_element_by_xpath(
-                '/html/body/div[1]/div[1]/div[1]/div/div[2]/div[4]/div[1]/span/div/div[1]/div').click()
+            try:
+                browser.find_element_by_xpath(
+                    '/html/body/div[1]/div[1]/div[1]/div/div[2]/div[4]/div[1]/span/div/div[1]/div').click()
+            except NoSuchElementException:
+                browser.find_element_by_xpath(
+                    '/html/body/div[1]/div/div[1]/div/div[2]/div[5]/div[1]/span/div/div[1]/div').click()
             time.sleep(float(decimal.Decimal(random.randrange(5, 7))))
             browser.get('https://www.facebook.com/me')
 
@@ -193,8 +197,12 @@ class Facebook():
             emit("message", {"status": True, "content": "Listing followers"})
             browser.set_window_size(900, 700)
             time.sleep(float(decimal.Decimal(random.randrange(4, 6))))
-            browser.find_element_by_xpath(
-                '/html/body/div[1]/div[1]/div[1]/div/div[2]/div[4]/div[1]/span/div/div[1]/div').click()
+            try:
+                browser.find_element_by_xpath(
+                    '/html/body/div[1]/div[1]/div[1]/div/div[2]/div[4]/div[1]/span/div/div[1]/div').click()
+            except NoSuchElementException:
+                browser.find_element_by_xpath(
+                    '/html/body/div[1]/div/div[1]/div/div[2]/div[5]/div[1]/span/div/div[1]/div').click()
             time.sleep(float(decimal.Decimal(random.randrange(5, 7))))
             browser.get('https://www.facebook.com/me')
             # browser.execute_script(
